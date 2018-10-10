8d7be728f0732c337574e22954d5d7592774eba9
ME <eng.sleiman@gmail.com>
2018-10-10 12:09:14 +0300

    Working well

--- GDSSTEMPLATE/main.Designer.vb
+++ GDSSTEMPLATE/main.Designer.vb
@@ -26,10 +26,28 @@ Partial Class main
         Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GiveGunToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GiveAmmoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ReportsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ByPersonToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.FullName_TextBox = New System.Windows.Forms.TextBox()
+        Me.Person_Label = New System.Windows.Forms.Label()
+        Me.Grade_ComboBox = New System.Windows.Forms.ComboBox()
+        Me.Source_ComboBox = New System.Windows.Forms.ComboBox()
+        Me.LName_CheckBox = New System.Windows.Forms.CheckBox()
+        Me.FATHER_CheckBox = New System.Windows.Forms.CheckBox()
+        Me.FName_CheckBox = New System.Windows.Forms.CheckBox()
+        Me.TextBoxLName = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox_FATHER = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.ComboBoxPersons = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox_FName = New System.Windows.Forms.TextBox()
+        Me.TextBoxMilitNb = New System.Windows.Forms.TextBox()
         Me.MenuStrip1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -37,7 +55,7 @@ Partial Class main
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(622, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(887, 24)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -60,30 +78,205 @@ Partial Class main
         Me.GiveAmmoToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
         Me.GiveAmmoToolStripMenuItem.Text = "Give ammo"
         '
+        'ReportsToolStripMenuItem
+        '
+        Me.ReportsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ByPersonToolStripMenuItem})
+        Me.ReportsToolStripMenuItem.Name = "ReportsToolStripMenuItem"
+        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
+        Me.ReportsToolStripMenuItem.Text = "Reports"
+        '
+        'ByPersonToolStripMenuItem
+        '
+        Me.ByPersonToolStripMenuItem.Name = "ByPersonToolStripMenuItem"
+        Me.ByPersonToolStripMenuItem.Size = New System.Drawing.Size(126, 22)
+        Me.ByPersonToolStripMenuItem.Text = "By Person"
+        '
         'ExitToolStripMenuItem
         '
         Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
         Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(135, 22)
         Me.ExitToolStripMenuItem.Text = "Exit"
         '
-        'ReportsToolStripMenuItem
+        'Panel2
         '
-        Me.ReportsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ByPersonToolStripMenuItem})
-        Me.ReportsToolStripMenuItem.Name = "ReportsToolStripMenuItem"
-        Me.ReportsToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.ReportsToolStripMenuItem.Text = "Reports"
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Panel2.Controls.Add(Me.FullName_TextBox)
+        Me.Panel2.Controls.Add(Me.Person_Label)
+        Me.Panel2.Controls.Add(Me.Grade_ComboBox)
+        Me.Panel2.Controls.Add(Me.Source_ComboBox)
+        Me.Panel2.Controls.Add(Me.LName_CheckBox)
+        Me.Panel2.Controls.Add(Me.FATHER_CheckBox)
+        Me.Panel2.Controls.Add(Me.FName_CheckBox)
+        Me.Panel2.Controls.Add(Me.TextBoxLName)
+        Me.Panel2.Controls.Add(Me.Label6)
+        Me.Panel2.Controls.Add(Me.TextBox_FATHER)
+        Me.Panel2.Controls.Add(Me.Label5)
+        Me.Panel2.Controls.Add(Me.ComboBoxPersons)
+        Me.Panel2.Controls.Add(Me.Label4)
+        Me.Panel2.Controls.Add(Me.Label3)
+        Me.Panel2.Controls.Add(Me.TextBox_FName)
+        Me.Panel2.Controls.Add(Me.TextBoxMilitNb)
+        Me.Panel2.Location = New System.Drawing.Point(12, 46)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(687, 181)
+        Me.Panel2.TabIndex = 5
         '
-        'ByPersonToolStripMenuItem
+        'FullName_TextBox
         '
-        Me.ByPersonToolStripMenuItem.Name = "ByPersonToolStripMenuItem"
-        Me.ByPersonToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.ByPersonToolStripMenuItem.Text = "By Person"
+        Me.FullName_TextBox.Enabled = False
+        Me.FullName_TextBox.Location = New System.Drawing.Point(412, 19)
+        Me.FullName_TextBox.Name = "FullName_TextBox"
+        Me.FullName_TextBox.Size = New System.Drawing.Size(211, 20)
+        Me.FullName_TextBox.TabIndex = 17
+        '
+        'Person_Label
+        '
+        Me.Person_Label.AutoSize = True
+        Me.Person_Label.Font = New System.Drawing.Font("Tahoma", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Person_Label.ForeColor = System.Drawing.Color.OrangeRed
+        Me.Person_Label.Location = New System.Drawing.Point(629, 21)
+        Me.Person_Label.Name = "Person_Label"
+        Me.Person_Label.Size = New System.Drawing.Size(18, 19)
+        Me.Person_Label.TabIndex = 16
+        Me.Person_Label.Text = "*"
+        Me.Person_Label.Visible = False
+        '
+        'Grade_ComboBox
+        '
+        Me.Grade_ComboBox.DisplayMember = "GRADE"
+        Me.Grade_ComboBox.FormattingEnabled = True
+        Me.Grade_ComboBox.Location = New System.Drawing.Point(309, 85)
+        Me.Grade_ComboBox.Name = "Grade_ComboBox"
+        Me.Grade_ComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.Grade_ComboBox.TabIndex = 14
+        Me.Grade_ComboBox.ValueMember = "GRADE"
+        Me.Grade_ComboBox.Visible = False
+        '
+        'Source_ComboBox
+        '
+        Me.Source_ComboBox.DisplayMember = "SOURCE"
+        Me.Source_ComboBox.FormattingEnabled = True
+        Me.Source_ComboBox.Location = New System.Drawing.Point(309, 52)
+        Me.Source_ComboBox.Name = "Source_ComboBox"
+        Me.Source_ComboBox.Size = New System.Drawing.Size(92, 21)
+        Me.Source_ComboBox.TabIndex = 13
+        Me.Source_ComboBox.ValueMember = "SOURCE"
+        Me.Source_ComboBox.Visible = False
+        '
+        'LName_CheckBox
+        '
+        Me.LName_CheckBox.AutoSize = True
+        Me.LName_CheckBox.Location = New System.Drawing.Point(204, 123)
+        Me.LName_CheckBox.Name = "LName_CheckBox"
+        Me.LName_CheckBox.Size = New System.Drawing.Size(58, 17)
+        Me.LName_CheckBox.TabIndex = 12
+        Me.LName_CheckBox.Text = "Enable"
+        Me.LName_CheckBox.UseVisualStyleBackColor = True
+        '
+        'FATHER_CheckBox
+        '
+        Me.FATHER_CheckBox.AutoSize = True
+        Me.FATHER_CheckBox.Location = New System.Drawing.Point(204, 94)
+        Me.FATHER_CheckBox.Name = "FATHER_CheckBox"
+        Me.FATHER_CheckBox.Size = New System.Drawing.Size(58, 17)
+        Me.FATHER_CheckBox.TabIndex = 11
+        Me.FATHER_CheckBox.Text = "Enable"
+        Me.FATHER_CheckBox.UseVisualStyleBackColor = True
+        '
+        'FName_CheckBox
+        '
+        Me.FName_CheckBox.AutoSize = True
+        Me.FName_CheckBox.Location = New System.Drawing.Point(204, 63)
+        Me.FName_CheckBox.Name = "FName_CheckBox"
+        Me.FName_CheckBox.Size = New System.Drawing.Size(58, 17)
+        Me.FName_CheckBox.TabIndex = 10
+        Me.FName_CheckBox.Text = "Enable"
+        Me.FName_CheckBox.UseVisualStyleBackColor = True
+        '
+        'TextBoxLName
+        '
+        Me.TextBoxLName.Enabled = False
+        Me.TextBoxLName.Location = New System.Drawing.Point(96, 116)
+        Me.TextBoxLName.Name = "TextBoxLName"
+        Me.TextBoxLName.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxLName.TabIndex = 9
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(10, 123)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(57, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Last Name"
+        '
+        'TextBox_FATHER
+        '
+        Me.TextBox_FATHER.Enabled = False
+        Me.TextBox_FATHER.Location = New System.Drawing.Point(96, 87)
+        Me.TextBox_FATHER.Name = "TextBox_FATHER"
+        Me.TextBox_FATHER.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox_FATHER.TabIndex = 7
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(10, 94)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(69, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Father Name"
+        '
+        'ComboBoxPersons
+        '
+        Me.ComboBoxPersons.DisplayMember = "FullName"
+        Me.ComboBoxPersons.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBoxPersons.FormattingEnabled = True
+        Me.ComboBoxPersons.Location = New System.Drawing.Point(99, 19)
+        Me.ComboBoxPersons.Name = "ComboBoxPersons"
+        Me.ComboBoxPersons.Size = New System.Drawing.Size(201, 21)
+        Me.ComboBoxPersons.TabIndex = 4
+        Me.ComboBoxPersons.ValueMember = "CODE"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(10, 61)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(58, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "First Name"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(9, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Military Number"
+        '
+        'TextBox_FName
+        '
+        Me.TextBox_FName.Enabled = False
+        Me.TextBox_FName.Location = New System.Drawing.Point(96, 61)
+        Me.TextBox_FName.Name = "TextBox_FName"
+        Me.TextBox_FName.Size = New System.Drawing.Size(101, 20)
+        Me.TextBox_FName.TabIndex = 1
+        '
+        'TextBoxMilitNb
+        '
+        Me.TextBoxMilitNb.Location = New System.Drawing.Point(306, 20)
+        Me.TextBoxMilitNb.Name = "TextBoxMilitNb"
+        Me.TextBoxMilitNb.Size = New System.Drawing.Size(100, 20)
+        Me.TextBoxMilitNb.TabIndex = 0
         '
         'main
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(622, 318)
+        Me.ClientSize = New System.Drawing.Size(887, 402)
+        Me.Controls.Add(Me.Panel2)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "main"
@@ -91,6 +284,8 @@ Partial Class main
         Me.Text = "Main"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -103,4 +298,21 @@ Partial Class main
     Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ReportsToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents ByPersonToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents FullName_TextBox As TextBox
+    Friend WithEvents Person_Label As Label
+    Friend WithEvents Grade_ComboBox As ComboBox
+    Friend WithEvents Source_ComboBox As ComboBox
+    Friend WithEvents LName_CheckBox As CheckBox
+    Friend WithEvents FATHER_CheckBox As CheckBox
+    Friend WithEvents FName_CheckBox As CheckBox
+    Friend WithEvents TextBoxLName As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox_FATHER As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents ComboBoxPersons As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox_FName As TextBox
+    Friend WithEvents TextBoxMilitNb As TextBox
 End Class
