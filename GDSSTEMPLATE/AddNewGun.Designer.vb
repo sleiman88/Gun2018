79c86a4a0c74cba1c4494c98fd82374f09a29301
ME <eng.sleiman@gmail.com>
2020-07-15 09:23:04 +0300

    Revert "test commist"
    This reverts commit 0512c836e764454d2471396c70e7c6ebd740c388.

--- GDSSTEMPLATE/AddNewGun.Designer.vb
+++ GDSSTEMPLATE/AddNewGun.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
 Partial Class AddNewGun
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()>
+    <System.Diagnostics.DebuggerNonUserCode()> _
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class AddNewGun
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()>
+    <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBox_gunName = New System.Windows.Forms.TextBox()
@@ -80,7 +80,7 @@ Partial Class AddNewGun
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(481, 240)
+        Me.ClientSize = New System.Drawing.Size(437, 313)
         Me.Controls.Add(Me.Label_er)
         Me.Controls.Add(Me.Label_error_double)
         Me.Controls.Add(Me.Button1)
